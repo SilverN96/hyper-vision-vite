4d70412f75b5219555b90593cb992edcdcb29b11
SilverN96 <150352993+SilverN96@users.noreply.github.com>
2025-07-21 22:29:38 +0300

    Add files via upload

---
+++ HyperVision_Vite_App/src/App.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hyper Vision – Hyper Vibe</h1>
+      <p>Streetwear with soul. 🔥</p>
+    </div>
+  )
+}
+
+export default App
